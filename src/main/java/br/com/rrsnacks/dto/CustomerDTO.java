2b2989e80a7f8bf6ab961fa5581b3fd3ae26720e
silvasantosrafael <rafael.silva318819@gmail.com>
2023-12-17 21:19:40 -0300

    initialize variable for avoid NPE

--- src/main/java/br/com/rrsnacks/dto/CustomerDTO.java
+++ src/main/java/br/com/rrsnacks/dto/CustomerDTO.java
@@ -3,6 +3,7 @@ package br.com.rrsnacks.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -11,6 +12,6 @@ public class CustomerDTO {
     private Long customerId;
     private String name;
     private String email;
-    private List<AddressDTO> addresses;
+    private List<AddressDTO> addresses = new ArrayList<>();
 }
 
