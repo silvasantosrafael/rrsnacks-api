52791a0ac03fa13788a836e09770f32b363ae320
silvasantosrafael <rafael.silva318819@gmail.com>
2024-06-22 03:03:42 -0300

    feat: Added new validations for image file

--- src/main/java/br/com/rrsnacks/service/FileUploadService.java
+++ src/main/java/br/com/rrsnacks/service/FileUploadService.java
@@ -6,25 +6,43 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 @Service
 public class FileUploadService {
     @Value("${server.media.upload.path}")
     private String basePath;
 
-    public String renameFile(MultipartFile imageFile) {
-        String[] splitName = imageFile.getOriginalFilename().split("\\.");
+    public String renameFile(MultipartFile imageFile) throws FileNotFoundException {
+        if (!isValidFile(imageFile)) {
+            throw new FileNotFoundException();
+        }
+
+        String imageName = Objects.requireNonNull(imageFile.getOriginalFilename());
+        String[] splitName = imageName.split("\\.");
         return splitName[0] + "_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmm")) + "." + splitName[1];
     }
 
     public boolean isValidFile(MultipartFile imageFile) {
-        if (imageFile.getOriginalFilename() == null || imageFile.getOriginalFilename().isEmpty()) {
+        if (imageFile == null) {
             return false;
         }
-        return imageFile.getContentType().startsWith("image");
+
+        String imageName = imageFile.getOriginalFilename();
+        if (imageName == null || imageName.isEmpty()) {
+            return false;
+        }
+
+        String contentType = imageFile.getContentType();
+        if (contentType == null || contentType.isEmpty()) {
+            return false;
+        }
+
+        return contentType.startsWith("image");
     }
 
     public void uploadFile(MultipartFile imageFile, String renameFile) throws FileUploadException {
