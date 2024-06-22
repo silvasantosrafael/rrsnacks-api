5c4b1bb4418e4624409dcd25fc9a186e0472d3c3
silvasantosrafael <rafael.silva318819@gmail.com>
2024-06-22 03:01:52 -0300

    feat: Class name changed and added annotation for avoid null fields

--- src/main/java/br/com/rrsnacks/exception/dto/ErrorDTO.java
+++ src/main/java/br/com/rrsnacks/exception/dto/ErrorDTO.java
@@ -1,11 +1,13 @@
-package br.com.rrsnacks.exception.handler;
+package br.com.rrsnacks.exception.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ErrorHandler {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorDTO {
     private String field;
     private String constraint;
     private String message;
