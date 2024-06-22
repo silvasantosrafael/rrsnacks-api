d6d193b8313a2817c1b29d2d0439d6c53a882f6b
silvasantosrafael <rafael.silva318819@gmail.com>
2024-06-22 03:12:10 -0300

    feat: Added property for image file and added annotation for avoid null values

--- src/main/java/br/com/rrsnacks/dto/SnackDTO.java
+++ src/main/java/br/com/rrsnacks/dto/SnackDTO.java
@@ -1,14 +1,17 @@
 package br.com.rrsnacks.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SnackDTO {
     private Long snackId;
     @NotNull
@@ -17,7 +20,7 @@ public class SnackDTO {
     @NotNull
     @Positive
     private BigDecimal price;
+    private MultipartFile imageFile;
     private String image;
-
 }
 
